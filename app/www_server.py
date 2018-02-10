12a552f7e647a242e4a3a63069a9e3a77260b92f
Tony Ting <k999hoshing@gmail.com>
2018-02-10 18:08:33 +0800

    Threaded server.

--- app/www_server.py
+++ app/www_server.py
@@ -15,4 +15,4 @@ def server_static(name="index.html"):
     return send_from_directory(app.static_folder, name)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80, threaded=True)
